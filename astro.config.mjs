0b9f15f58095b57c3e015523744034e5d2fff2ab
GrantBirki <grantbirki@github.com>
2023-10-18 16:01:59 -0600

    add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,25 @@ import react from '@astrojs/react';
 import bookshop from '@bookshop/astro-bookshop';
 import sitemap from '@astrojs/sitemap';
 import prefetch from '@astrojs/prefetch';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gldnfloral.com/',
-  integrations: [bookshop(), react(), sitemap(), prefetch()],
+  integrations: [
+    bookshop(),
+    react(),
+    sitemap(),
+    prefetch(),
+    compress({
+      CSS: true,
+      HTML: {
+        removeAttributeQuotes: false,
+      },
+      Image: false,
+      JavaScript: true,
+      SVG: true,
+      Logger: 1,
+    }),
+  ],
 });
