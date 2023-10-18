e23236411da57ba7efe2429bb0ce1711e7385198
GrantBirki <grantbirki@github.com>
2023-10-18 15:47:52 -0600

    add sitemap, CNAME, and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import bookshop from '@bookshop/astro-bookshop';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gldnfloral.com/',
-  integrations: [bookshop(), react()],
+  integrations: [bookshop(), react(), sitemap()],
 });
